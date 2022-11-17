9a3b5ed33fc5b14561bcc6202b7c89bbe27ddc76
NestorDanielGomez <nestordanielgomez@gmail.com>
2022-11-16 23:23:48 -0300

    actualiza cliente

--- src/data/clientes.js
+++ src/data/clientes.js
@@ -21,7 +21,21 @@ try {
 }
 
 export async function ObtenerCliente(id) {
-  const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/{id}`);
+  const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`);
   const resultado = await respuesta.json();
   return resultado;
+}
+
+export async function ActualizarCliente(id, datos) {
+try {
+  const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`, {method:"PUT",
+  body: JSON.stringify(datos),
+  headers:{
+    "Content-Type":"application/json"
+  } });
+  await respuesta.json()
+
+} catch (error) {
+  
+}
 }
